c3bec2bbf7fa0f20ee7f0351d26c3ae0d86a2440
PARTH101-netizen <kr8664206@gmail.com>
2024-07-28 12:08:42 +0530

    Update Effects.js

--- src/Effects.js
+++ src/Effects.js
@@ -1,7 +1,7 @@
 import { useLoader } from '@react-three/fiber';
 import { EffectComposer, SSR, Bloom, LUT, LUTCubeLoader} from '@react-three/postprocessing';
 import { useControls } from 'leva';
-export const {LUTCubeLoader } = PostProcessing;
+export const {LUTCubeLoader } ;
 export function Effects() {
   const texture = useLoader( LUTCubeLoader , '/F-6800-STD.cube');
   const { enabled, ...props } = useControls({
